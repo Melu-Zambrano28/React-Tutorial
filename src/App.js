bba0518665361048a32f3977754680767bae9182
Melissa zambrano <belenzambrano@hotmail.es>
2019-01-04 17:45:45 +0100

    Leccion : State and prop de react . Se simulò el tratamiento de los datos del backend con un file json , en el componente de react guardamos como estado de modo de poderlos pedir dentro del render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './components/Navigation'
+import Navigation from './components/Navigation';
+import { task } from './task.json';
 
+console.log(task);
 class App extends Component {
+  constructor(){ // el metodo constructor viene ejecutador primero del render
+    super(); //hereda toda la funcionalidad de react
+    this.state ={
+      tareas : task
+    }
+  }
+
+
   render() {
+    //Primero de renderizar procesamos los datos
+
+   const tareasList =  this.state.tareas.map((tareas, i ) => { //por cada tarea va a retornar una carta , 
+
+
+      return(
+
+      <div className="col-md-4">
+          <div className="card mt-4">
+            <div className="card-header">
+               <h3>{tareas.title}</h3>
+               <span className="badge badge-pill badge-danger ml-2">
+                  {tareas.priority}
+               </span>
+            </div>
+            <div className="card-body">
+              <p>{tareas.description}</p>
+              <p>{tareas.responsible}</p>
+            </div>
+        </div>
+
+      </div>
+       
+
+      )
+    })
+
+
+    /*return del render, aqui se muestran los datos ya procesados (variable tareaList) */
     return (
       <div className="App">
-      <Navigation titutloDinamico = "Mi primera prop en react"/>
-       <img src={logo} className='App-logo' alt='logo'></img>
-      <Navigation titutloDinamico = "Mi segunda prop del segundo componente react"/>
+         <nav className="navbar navbar-dark bg-dark">
+                <a href="" className="text-white">
+                  Task
+                  <span className="badge badge-pill badge-light ml-2">
+                    {this.state.tareas.length}
+                  </span>
+                </a>
+          </nav>
+      <div className="container">
+        <div className="row mt-4">
+          {tareasList} 
+        </div>
+
+      </div>
+        <img src={logo} className='App-logo' alt='logo'></img>
+    
       </div>
     );
   }
